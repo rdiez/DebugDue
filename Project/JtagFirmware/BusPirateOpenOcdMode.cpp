5caccc252208b30a677b58725f2f1b6f41dc362f
R. Diez <rdiezmail-arduino@yahoo.de>
2013-06-14 19:04:15 +0200

    JTAG pin definitions moved to a separate file.

--- Project/JtagFirmware/BusPirateOpenOcdMode.cpp
+++ Project/JtagFirmware/BusPirateOpenOcdMode.cpp
@@ -27,6 +27,7 @@
 #include "BusPirateConnection.h"
 #include "BusPirateBinaryMode.h"
 #include "Globals.h"
+#include "JtagPins.h"
 
 
 #define OPEN_OCD_CMD_CODE_LEN         1
@@ -112,62 +113,6 @@ enum
 
 static JtagPinModeEnum s_pinMode;
 static bool s_pullUps;
-
-// JTAG pins, 10-pin connector.
-// This is the same layout as the Altera USB Blaster connector with Atmel AVR's additions:
-// Legend: Arduino Pin number (printed on the board) / Port pin / USB Blaster Pin number
-//   42/PA19/09 TDI | 43/PA20/10 GND2
-//   44/PC19/07  -  | 45/PC18/08 nTRST
-//   46/PC17/05 TMS | 47/PC16/06 nSRST
-//   48/PC15/03 TDO | 49/PC14/04 VCC
-//   50/PC13/01 TCK | 51/PC12/02 GND1
-// Note that there is a console command to list these pins.
-// None of the Arduino Due pins selected above has any other alternate function (like SPI bus)
-// listed on the Arduino source code (hardware/arduino/sam/variants/arduino_due_x/variant.cpp).
-//
-// Older pinout I used, it was too close to the 5V pins at the edge.
-// Legend: Arduino Pin number / Chip pin number / Port pin number
-//   31/26/PA07 TDI | 30/22/PD09 GND1
-//   29/19/PD06  -  | 28/16/PD03  -
-//   27/15/PD02 TMS | 26/14/PD01  -
-//   25/13/PD00 TDO | 24/08/PA15 VCC
-//   23/07/PA14 TCK | 22/01/PB26 GND2
-
-
-// JTAG data signals.
-
-#define JTAG_TDI_PIO  PIOA
-#define JTAG_TDI_PIN  19
-
-#define JTAG_TMS_PIO  PIOC
-#define JTAG_TMS_PIN  17
-
-#define JTAG_TDO_PIO  PIOC
-#define JTAG_TDO_PIN  15
-
-#define JTAG_TCK_PIO  PIOC
-#define JTAG_TCK_PIN  13
-
-
-// JTAG reset signals.
-
-#define JTAG_TRST_PIO  PIOC
-#define JTAG_TRST_PIN  18
-
-#define JTAG_SRST_PIO  PIOC
-#define JTAG_SRST_PIN  16
-
-
-// JTAG voltage signals.
-
-#define JTAG_VCC_PIO  PIOC
-#define JTAG_VCC_PIN  14
-
-#define JTAG_GND1_PIO  PIOC
-#define JTAG_GND1_PIN  12
-
-#define JTAG_GND2_PIO  PIOA
-#define JTAG_GND2_PIN  20
 
 
 static void ConfigureJtagPins ( void )
@@ -514,7 +459,7 @@ static uint8_t ShiftSeveralBits ( const uint8_t tdi8,
     DbgconPrint( "TDI8: 0x%02X, TMS8: 0x%02X, TDO8: 0x%02X" EOL, tdi8, tms8, tdo8 );
 
 
-  // Note that OpenOCD 0.7.0's Bus Pirate driver does not bother clearing the last buffer
+  // Note that OpenOCD 0.8.0's Bus Pirate driver does not bother clearing the last buffer
   // contents before sending a new one, so, if the bit count is not a multiple of 8,
   // the last TDI and TMS bits may not be zero, they may be rubbish from the data previously sent.
   // However, I have changed my OpenOCD locally to clear those bits, I intend to submit a patch soon.
---
+++ Project/JtagFirmware/JtagPins.h
@@ -0,0 +1,63 @@
+// Include this header file only once.
+#ifndef JTAG_PINS_H_INCLUDED
+#define JTAG_PINS_H_INCLUDED
+
+
+// JTAG pins, 10-pin connector.
+// This is the same layout as the Altera USB Blaster connector with Atmel AVR's additions:
+// Legend: Arduino Pin number (printed on the board) / Port pin / USB Blaster Pin number
+//   42/PA19/09 TDI | 43/PA20/10 GND2
+//   44/PC19/07  -  | 45/PC18/08 nTRST
+//   46/PC17/05 TMS | 47/PC16/06 nSRST
+//   48/PC15/03 TDO | 49/PC14/04 VCC
+//   50/PC13/01 TCK | 51/PC12/02 GND1
+// Note that there is a console command to list these pins.
+// None of the Arduino Due pins selected above has any other alternate function (like SPI bus)
+// listed on the Arduino source code (hardware/arduino/sam/variants/arduino_due_x/variant.cpp).
+//
+// Older pinout I used, it was too close to the 5V pins at the edge.
+// Legend: Arduino Pin number / Chip pin number / Port pin number
+//   31/26/PA07 TDI | 30/22/PD09 GND1
+//   29/19/PD06  -  | 28/16/PD03  -
+//   27/15/PD02 TMS | 26/14/PD01  -
+//   25/13/PD00 TDO | 24/08/PA15 VCC
+//   23/07/PA14 TCK | 22/01/PB26 GND2
+
+
+// JTAG data signals.
+
+#define JTAG_TDI_PIO  PIOA
+#define JTAG_TDI_PIN  19
+
+#define JTAG_TMS_PIO  PIOC
+#define JTAG_TMS_PIN  17
+
+#define JTAG_TDO_PIO  PIOC
+#define JTAG_TDO_PIN  15
+
+#define JTAG_TCK_PIO  PIOC
+#define JTAG_TCK_PIN  13
+
+
+// JTAG reset signals.
+
+#define JTAG_TRST_PIO  PIOC
+#define JTAG_TRST_PIN  18
+
+#define JTAG_SRST_PIO  PIOC
+#define JTAG_SRST_PIN  16
+
+
+// JTAG voltage signals.
+
+#define JTAG_VCC_PIO  PIOC
+#define JTAG_VCC_PIN  14
+
+#define JTAG_GND1_PIO  PIOC
+#define JTAG_GND1_PIN  12
+
+#define JTAG_GND2_PIO  PIOA
+#define JTAG_GND2_PIN  20
+
+  
+#endif  // Include this header file only once.
