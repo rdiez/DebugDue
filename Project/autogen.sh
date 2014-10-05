061d1c5eaabbbcae6a05cca3a0ab0f0f9d76dc39
R. Diez <rdiezmail-arduino@yahoo.de>
2014-10-05 21:55:58 +0200

    Create the "m4" subdir before calling atuoreconf.

--- Project/autogen.sh
+++ Project/autogen.sh
@@ -3,7 +3,14 @@
 set -o errexit
 set -o pipefail
 set -o nounset
-set -o posix
+
+# Without this line in the top-level Makefile.am:
+#   ACLOCAL_AMFLAGS = -I m4
+# you get this warning:
+#   libtoolize: Consider adding `-I m4' to ACLOCAL_AMFLAGS in Makefile.am.
+# However, if we add that line, autoreconf does not work if you don't manually create
+# the m4 subdirectory beforehand.
+mkdir --parents "m4"
 
 CMD="autoreconf --warnings=all --install"
 printf "$CMD\n\n"
