22de47ba3901f777159100386757fd04f675f488
R. Diez <rdiezmail-arduino@yahoo.de>
2015-05-11 23:10:12 +0200

    Added comment about AUTOMAKE_JOBS.

--- Project/autogen.sh
+++ Project/autogen.sh
@@ -12,6 +12,10 @@ set -o nounset
 # the m4 subdirectory beforehand.
 mkdir --parents "m4"
 
+# We could speed automake up by setting AUTOMAKE_JOBS like this:
+#  export AUTOMAKE_JOBS="$(( $(getconf _NPROCESSORS_ONLN) + 1 ))"
+# However, this feature is still marked as experimental in automake's 1.15 manual.
+
 CMD="autoreconf --warnings=all --install"
 printf "$CMD\n\n"
 eval "$CMD"
