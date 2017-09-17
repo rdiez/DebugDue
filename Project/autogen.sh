3794d33c791fe674939f29a9f8d15a9e1542f570
R. Diez <rdiezmail-arduino@yahoo.de>
2017-09-17 17:11:54 +0200

    Subdir "m4" removed, as only libtool was using it, and we are not using libtool anymore.

--- Project/autogen.sh
+++ Project/autogen.sh
@@ -4,14 +4,6 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-# Without this line in the top-level Makefile.am:
-#   ACLOCAL_AMFLAGS = -I m4
-# you get this warning:
-#   libtoolize: Consider adding `-I m4' to ACLOCAL_AMFLAGS in Makefile.am.
-# However, if we add that line, autoreconf does not work if you don't manually create
-# the m4 subdirectory beforehand.
-mkdir --parents "m4"
-
 # We could speed automake up by setting AUTOMAKE_JOBS like this:
 #  export AUTOMAKE_JOBS="$(( $(getconf _NPROCESSORS_ONLN) + 1 ))"
 # However, this feature is still marked as experimental in automake's 1.15 manual.
