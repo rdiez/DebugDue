2c4a7f79abe42617a1245e06c021b9cfb20ede81
R. Diez <rdiezmail-arduino@yahoo.de>
2026-06-20 11:22:32 +0200

    Only indentation.

--- Project/src/BareMetalSupport/BoardInitUtils.cpp
+++ Project/src/BareMetalSupport/BoardInitUtils.cpp
@@ -220,12 +220,12 @@ void RuntimeStartupChecks ( void ) throw()
   // I haven't patched strerror() in Picolibc yet.
   #ifndef __PICOLIBC__
 
-  // Check whether the patch to remove all strerror() strings is working properly.
-  // "n/a" means "not available".
-  if ( 0 != strcmp( "<n/a>", strerror( ENOENT ) ) )
-  {
-    Panic( "strerror() does not deliver the expected patched string." );
-  }
+    // Check whether the patch to remove all strerror() strings is working properly.
+    // "n/a" means "not available".
+    if ( 0 != strcmp( "<n/a>", strerror( ENOENT ) ) )
+    {
+      Panic( "strerror() does not deliver the expected patched string." );
+    }
 
   #endif  // #ifndef __PICOLIBC__
 }
